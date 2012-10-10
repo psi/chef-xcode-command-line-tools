7bb072072ae8f1e3fd22ad3bb73a724ffad3e756
JD Harrington <jd@jdharrington.net>
2012-10-10 01:13:10 -0400

    Now installable without mpkg_name in dmg cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,12 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 
-dmg_package "xcode-command-line-tools" do
+dmg_package "Command Line Tools (Mountain Lion)" do
   source "https://dl.dropbox.com/u/211124/xcode451cltools_10_86938200a.dmg"
   checksum "c640cf70063859c0d1af4bf592ecce57"
+  dmg_name "Command Line Tools (Mountain Lion)"
   volumes_dir "Command Line Tools (Mountain Lion)"
   type "mpkg"
-  mpkg_name "Command Line Tools (Mountain Lion)"
 
-  creates "/usr/bin/make"
+  creates "/usr/bin/gcc"
 end
